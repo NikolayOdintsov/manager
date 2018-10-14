9bee1555f84d23d1d84f3a45c2d00163d8841864
Nikolay Odintsov <nikolay.odintsov@akelius.de>
2018-10-14 22:29:35 +0200

    Added saving employee data to Firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,8 +1,22 @@
+import firebase from 'firebase';
+import {Actions} from 'react-native-router-flux';
 import {EMPLOYEE_UPDATE} from './types';
 
 export const employeeUpdate = ({prop, value}) => {
-    return{
+    return {
         type: EMPLOYEE_UPDATE,
         payload: {prop, value}
+    };
+};
+
+export const employeeCreate = ({name, phone, shift}) => {
+    const {currentUser} = firebase.auth();
+
+    //No need in 'redux-thung' 'dispatch' as we don't care about response.
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .push({name, phone, shift})
+            //Actions.pop() - to just return without 'Back' button
+            .then(() => Actions.pop());
     };
 };
