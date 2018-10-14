1300be05cb926366dfe86955a9b03c2198850537
Nikolay Odintsov <nikolay.odintsov@akelius.de>
2018-10-14 19:11:30 +0200

    Completed login form

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,8 +1,17 @@
-import {EMAIL_CHANGED, PASSWORD_CHANGED} from '../actions/types';
+import {
+    EMAIL_CHANGED,
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL,
+    LOGIN_USER
+} from '../actions/types';
 
 const INITIAL_STATE = {
     email: '',
-    password: ''
+    password: '',
+    user: null,
+    error: '',
+    loading: false
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -19,6 +28,18 @@ export default (state = INITIAL_STATE, action) => {
 
         case PASSWORD_CHANGED:
             return {...state, password: action.payload};
+
+        case LOGIN_USER:
+            return {...state, loading: true, error: ''};
+
+        case LOGIN_USER_SUCCESS:
+            /**
+             * ...INITIAL_STATE  -> create new object of state + fill in values with INITIAL_STATE
+             */
+            return {...state, ...INITIAL_STATE, user: action.payload};
+
+        case LOGIN_USER_FAIL:
+            return {...state, error: 'Authentication failed.', loading: false, password: ''};
 
         default:
             return state;
